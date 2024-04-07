6cbc18def7ab1425e1e80f1622da42d8f084037d
Iann-Monteiro <iann.monteiro@softfocus.com.br>
2024-04-07 02:07:50 -0300

    alua don don don

---
+++ src/main/java/com/ianndev/workshop/service/UserService.java
@@ -0,0 +1,55 @@
+package com.ianndev.workshop.service;
+
+import com.ianndev.workshop.domain.User;
+import com.ianndev.workshop.dto.UserDTO;
+import com.ianndev.workshop.repository.UserRepository;
+import com.ianndev.workshop.service.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService implements Serializable {
+
+    public static final long serialVersionUID = 1L;
+
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll() {
+        return repo.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public void delete(String id) {
+        findById(id);
+        repo.deleteById(id);
+    }
+
+    public User update(User obj){
+        User newObj = repo.findById(obj.getId()).get();
+        updateData(newObj, obj);
+        return repo.save(newObj);
+
+    }
+
+    private void updateData(User newObj, User obj) {
+        newObj.setName(obj.getName());
+        newObj.setEmail(obj.getEmail());
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+    }
+}
---
+++ src/main/java/com/ianndev/workshop/service/exception/ObjectNotFoundException.java
@@ -0,0 +1,8 @@
+package com.ianndev.workshop.service.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+    public static final long serialVersionUID = 1L;
+    public ObjectNotFoundException(String message) {
+        super(message);
+    }
+}
